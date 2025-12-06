eeef1caa8ff4dad4cdf125fd361ea96d5ddd4489
Malik-Abhinav <abhinavmalik1573@gmail.com>
2025-12-05 16:53:48 -0800

    feature(core): implement zero-gc Event and EventPool with unit tests

--- src/main/java/com/kestrel/core/EventPool.java
+++ src/main/java/com/kestrel/core/EventPool.java
@@ -1,3 +1,24 @@
 package com.kestrel.core;
 
-public class EventPool {}
+public class EventPool {
+    private final Event[] pool;
+    private int index;
+    private final int size;
+
+    public EventPool(int size){
+        this.size = size;
+        this.pool = new Event[size];
+
+        for (int i = 0; i<size; i++){
+            pool[i] = new Event();
+        }
+    }
+
+    public Event get(){
+        if (index == size){
+            index = 0;      // wrap around
+        }
+        return pool[index++];
+    }
+
+}
